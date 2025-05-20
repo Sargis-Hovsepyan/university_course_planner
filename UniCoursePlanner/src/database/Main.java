fff42c1d3b1a89884d382e86cd402470103183a8
Sargis <sargishovsepyan2001@gmail.com>
2025-05-21 01:25:51 +0400

    feat(db): Initializing the database

--- UniCoursePlanner/src/database/Main.java
+++ UniCoursePlanner/src/database/Main.java
@@ -1,4 +1,34 @@
 package database;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DatabaseManager.getConnection();
+        StartUp startup = StartUp.getInstance();
+
+        Tables.setConnection(conn);
+
+        /* Inserting Data */
+
+        String projectRoot = System.getProperty("user.dir");
+        Path parentDir = Paths.get(projectRoot).getParent();
+
+        Path pathPrograms = parentDir.resolve("data/university_degree_programs.csv");
+        startup.readDegreePrograms(pathPrograms.toString());
+
+        Path pathInstructors = parentDir.resolve("data/university_instructors.csv");
+        startup.readInstructors(pathInstructors.toString());
+
+        Path pathCourses = parentDir.resolve("data/university_courses.csv");
+        startup.readCourses(pathCourses.toString());
+
+        Path pathSchedules = parentDir.resolve("data/university_schedules.csv");
+        startup.readSchedules(pathSchedules.toString());
+
+        conn.close();
+    }
 }
