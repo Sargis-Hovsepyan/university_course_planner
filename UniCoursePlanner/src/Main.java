39380a7ee7b9ae837cfa408a0e7c87180b5d3333
mher270601 <41270907+mher270601@users.noreply.github.com>
2025-05-21 21:21:26 +0400

    Revert "Feature/cli support gev"

--- UniCoursePlanner/src/Main.java
+++ UniCoursePlanner/src/Main.java
@@ -1,14 +1,7 @@
-package service;
-
-import service.gemini.GeminiClient;
-
-// THIS IS NOT FINAL MAIN. JUST A TEST
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        GeminiClient geminiClient = new GeminiClient();
-        String prompt = "Tell me the capital of France";
-        String response = geminiClient.sendPrompt(prompt);
-        System.out.println("Gemini Response:\n" + response);
+        System.out.println("Development in Progress...");
+        }
     }
-}
